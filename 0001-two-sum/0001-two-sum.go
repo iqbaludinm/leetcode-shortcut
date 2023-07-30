af93fe16c97f5e0fb95464e97e837fabe4b1269b
Muhammad Iqbaludin Zaky <55065320+iqbaludinm@users.noreply.github.com>
2023-07-30 21:17:00 +0700

    Time: 3 ms (95.68%), Space: 4.3 MB (13.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,11 +1,11 @@
 func twoSum(nums []int, target int) []int {
-    var temp []int
-    for idxI, i := range nums {
-      for idxJ, j := range nums {
-          if i + j == target && idxI != idxJ {
-              return append(temp, idxI, idxJ)
-          }
-      }
-    }
-    return temp
+    temp := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		complement := target - nums[i]
+		if value, exist := temp[complement]; exist {
+			return []int{value, i}
+		}
+		temp[nums[i]] = i
+	}
+	return []int{}
 }
